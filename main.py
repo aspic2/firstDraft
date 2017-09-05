5cdc93d59e7e565c46c2bb2f8e886c98a6eb8486
MThomp <mthomp@protonmail.com>
2017-09-04 23:06:25 -0500

    need to set up draft round logic. need to start creating modules for the classes.

--- main.py
+++ main.py
@@ -60,9 +60,10 @@ GENERAL SCORING SETTINGS
 
 Use Fractional Points: Yes
 Use Negative Points: Yes
-
 """
 
+
+import statistics
 import sqlite3
 from os import getcwd
 
@@ -76,6 +77,24 @@ Fantasy_Points FROM Kickers WHERE Season = 2015
 offense_query = '''SELECT Season, Position, FirstName, LastName, Team, \
 Fantasy_Points FROM Offense WHERE Season = 2015
 '''
+
+
+class Draft(object):
+
+    def __init__(self, teams, rounds=15):
+        self.rounds = rounds
+        self.teams = teams
+        drafts_per_round = len(self.teams)
+
+    #def draft(self, teams, current_round):
+    #    if current_round <= self.rounds:
+
+
+
+
+
+
+
 
 class Team(object):
 
@@ -97,9 +116,17 @@ class Player(object):
         self.position = position
         self.name = name
         self.nfl_team = nfl_team
-        self.point = points
+        self.points = points
+        self.owner = None
+
 
 class Data(object):
+
+    def __init__(self):
+        self.mean = 0
+        self.median = 0
+        self.mode = 0
+        self.standard_deviation = 0
 
     def get_data(query):
         conn = sqlite3.connect('football.db')
@@ -114,23 +141,113 @@ class Data(object):
             # switch for the 3 different score sysems
             # names are tuples in format (firstName, lastName)
             if query == offense_query:
-                players.append(Player(stat[1], (stat[2], stat[3]), stat[4], stat[5]))
+                players.append(Player(
+                stat[1], (stat[2], stat[3]), stat[4], stat[5]))
             elif query == defense_query:
-                players.append(Player(stat[1], (stat[2], stat[3]), stat[3], stat[4]))
+                players.append(Player(
+                stat[1], (stat[2], stat[3]), stat[3], stat[4]))
             elif query == kickers_query:
-                players.append(Player(stat[1], (stat[2], stat[3]), stat[4], stat[5]))
+                players.append(Player(
+                stat[1], (stat[2], stat[3]), stat[4], stat[5]))
             else:
                 print("Was unable to create players using data.")
                 return [-1]
-        for player in players:
-            print(player.name)
+        #for player in players:
+        #    print(player.name)
         return players
+
+    def do_stats(a_list):
+        pass
+
+# make lambdas for this
+    def get_mean(a_list):
+        X = []
+        for item in a_list:
+            X.append(item.points)
+        return statistics.mean(X)
+
+    def get_median(a_list):
+        X = []
+        for item in a_list:
+            X.append(item.points)
+        return statistics.median_low(X)
+
+    def get_mode(a_list):
+        X = []
+        for item in a_list:
+            X.append(item.points)
+        return statistics.mode(X)
+
+    def get_sd(a_list):
+        X = []
+        for item in a_list:
+            X.append(item.points)
+        return statistics.pstdev(X)
+
+
+
+class PlayerRepo(object):
+    """Use this to store master list of all players and manage availability."""
+    def __init__(self):
+        self.all_positions = []
+        self.defs = []
+        self.ks = []
+        self.rbs = []
+        self.qbs = []
+        self.tes = []
+        self.wrs = []
+        self.position_dict = {"DEF": self.defs, "K": self.ks, "RB": self.rbs,
+        "QB": self.qbs, "TE": self.tes, "WR": self.wrs}
+
+    def insert_players(self, players):
+        for p in players:
+            self.all_positions.append(p)
+
+    def create_position_lists(self):
+        for p in self.all_positions:
+            correct_list = self.position_dict[p.position]
+            correct_list.append(p)
+
+
+class PlayersList(list):
+    """Class to store important statistical data regarding
+    desirability of a certain player during a certain draft round.
+    """
+
+    def __init__(self, input_list):
+        self.players = input_list
+
+    def do_stats(self):
+        self.mean = statistics.mean(self.players)
+        self.median = statistics.median_low(self.players)
+        self.mode = statistics.mode(self.players)
+        self.standard_deviation = statistics.pstdev(self.players)
+
+
 
 
 def main():
+    repo = PlayerRepo()
     defenses = Data.build_players(defense_query)
     offenses = Data.build_players(offense_query)
     kickers = Data.build_players(kickers_query)
+
+    repo.insert_players(defenses)
+    repo.insert_players(offenses)
+    repo.insert_players(kickers)
+    repo.create_position_lists()
+    for p in repo.rbs:
+        print(p.name)
+
+
+    kickers_sd = Data.get_sd(repo.ks)
+    rb_sd = Data.get_sd(repo.rbs)
+    wr_sd = Data.get_sd(repo.wrs)
+    qb_sd = Data.get_sd(repo.qbs)
+    print(kickers_sd)
+    print(rb_sd)
+    print(wr_sd)
+    print(qb_sd)
 
 
 if __name__ == '__main__':
