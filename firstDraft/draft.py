b54787c09fb885db4d5293b38e9d8b9d5622c9f4
MThomp <mthomp@protonmail.com>
2017-10-25 13:50:19 -0500

    working on Draft.start()

--- firstDraft/draft.py
+++ firstDraft/draft.py
@@ -1,9 +1,24 @@
 
 from firstDraft.team import Team
+from firstDraft.player_repo import PlayerRepo
 
 
 class Draft(object):
 
     def __init__(self, teams_list):
         self.teams = teams_list
+        self.repo = PlayerRepo().fill_list()
+
+    def round_of_drafts(self):
+        for t in self.teams:
+            # TODO: fix hard-coding of selected player
+            # TODO: ensure both t.draft_player() and repo.draft_player() happen!
+            # TODO: potential for discrepancy in available players currently
+            draft_candidate = self.repo[0]
+            t.draft_player(self.repo[0])
+            self.repo.draft_player(draft_candidate)
+        return self
+
+    def start(self):
+        pass
 
--- tests/test_draft.py
+++ tests/test_draft.py
@@ -14,11 +14,20 @@ class TestDraft(unittest.TestCase):
     def test_teams(self):
         self.assertTrue(len(self.draft.teams) == 2)
 
-    def test_selection_round(self):
+    def test_round_of_drafts(self):
         starting_no_of_players = len(self.draft.teams[0])
         # test that each team gets to pick a player during a round.
         # test that invalid selections are handled properly and team picks again
-        self.draft.selection_round()
+        self.draft.round_of_drafts()
         self.assertTrue(len(self.draft.teams[0])) == starting_no_of_players + 1
+        # check that second team also had a draft
+        self.assertTrue(len(self.draft.teams[1])) == starting_no_of_players + 1
+
+    def test_start(self):
+        self.draft.start()
+        self.assertEqual(len(self.draft.teams[0]), len(self.draft.teams[1]))
+        # TODO: set up Draft() to complete a pre-determined number of rounds
+        self.assertEqual(self.draft.number_of_rounds, len(self.draft.teams[-1]))
+
 
 
